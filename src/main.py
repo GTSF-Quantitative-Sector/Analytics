4fbbcec54414b9770eb0aa8f25902928aa23446d
BKing28 <braydenking@gatech.edu>
2026-02-23 12:26:46 -0500

    Added data

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ from utils import *
 
 def main():
     portfolio_df = parse_xls("data/2026S Portfolio Holdings and Analytics.xlsx")
-    portfolio_df.to_csv("data/output.csv")
 
 if __name__ == "__main__":
     main()
